1781622e2408d179a258d69f1495ab6da9ddbd00
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-09-27 12:36:47 +0100

    Progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,19 @@ import '../styles/globals.css'
 import Layout from "../components/Layout/Layout";
 import {Provider} from "react-redux";
 import {store} from "../redux/app/store";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router"
+
+const progress = new ProgressBar({
+    size: 3,
+    color: "#111827",
+    className: "z-1",
+    delay: 100
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({Component, pageProps}) {
     return <Provider store={store}>
